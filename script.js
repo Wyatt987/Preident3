4eaf79bd1aa804f4ef383ae908ecadda92c297ae
Wyatt987 <526wbedor@frhsd.com>
2025-02-27 00:40:09 +0000

    Ask Kilgore for help on z-index

---
+++ script.js
@@ -0,0 +1,9 @@
+let texas = document.getElementById('texas')
+let behind = document.getElementById('behind')
+
+document.getElementById('texas').addEventListener('click', function() {
+    texas.classList.add('animate');
+    behind.classList.remove('d-none');
+} )
+
+
