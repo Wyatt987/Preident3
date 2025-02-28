007f5da5191d80b028066e973241dce82c9adc29
Wyatt987 <526wbedor@frhsd.com>
2025-02-28 00:53:50 +0000

    Finished Presidency

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let texas = document.getElementById('texas')
-let behind = document.getElementById('behind')
+//Moves Texas image to right on click
+let texas = document.getElementById('texas');
+let behind = document.getElementById('behind');
 
-document.getElementById('texas').addEventListener('click', function() {
+texas.addEventListener('click', function() {
     texas.classList.add('animate');
     behind.classList.remove('d-none');
-} )
+});
 
+//Moves veto image to left on click
+let veto = document.getElementById('veto');
+let behindVeto = document.getElementById('behindVeto');
 
+veto.addEventListener('click', function () {
+  veto.classList.add('animate-veto');
+  behindVeto.classList.remove('d-none');
+});
